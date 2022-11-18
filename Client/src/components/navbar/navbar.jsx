6c2d6c8e3ce3692a1ae3f55d5064c6898b392fcd
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-11-18 21:45:55 +0300

    simple navbar

---
+++ Client/src/components/navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../../assets/css/navbar.css"
+function Navbar() {
+  return (
+    <div>
+       <div>
+         <nav>
+         <a href="#"> E-chat</a>
+           <a href='#'> Profile </a>
+         <a href='#'>A propos</a>
+
+                  
+         </nav>
+      </div>
+         
+    </div>
+  )
+}
+
+export default Navbar
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # React_Chat_App
-creation d'un chatApp <'Chat-Naka🤯💻'> en temps reel avec react
+creation d'un chatApp <'e-chat🤯💻'> en temps reel avec react
+
+
+#swup
+
+
+
+import Swup from 'swup';
+const swup = new Swup(); // only this line when included with script tag
