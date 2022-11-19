3387c1a1013885f1fa64901f0b3535a9ed7885c7
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-11-19 22:56:17 +0300

    second modif home

--- Client/src/components/home/home.jsx
+++ Client/src/components/home/home.jsx
@@ -3,6 +3,7 @@ import "../../assets/css/home.css";
 import home2 from "../../assets/img/icon.jpg"
 import home from "../../assets/svg/undraw_group_hangout_re_4t8r.svg";
 import backgrounds from "../../assets/svg/wave.svg"
+import git from "../../assets/ico/git.ico"
 function Home() {
   return (
     <div>
@@ -18,7 +19,7 @@ function Home() {
             </h2>
             <p>Une appli 100% malgache et 100% gratuite</p>
              <br/>
-             <h6> @Project github finoana randria</h6>
+             <h6> @Project github finoana randria <img src={git} className="icons"/> </h6>
           </div>
         </div>
       </div>
