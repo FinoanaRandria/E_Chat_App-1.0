c8f56dca1993a7252a93dff5f57ba43a826380e5
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-11-20 10:31:24 +0300

    ajout de commposant

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import{BrowserRouter,Routes,Route,Link,useNavigate} from "react-router-dom"
 import './App.css'
 import Navbar from './components/navbar/navbar'
 import Home from './components/home/home'
