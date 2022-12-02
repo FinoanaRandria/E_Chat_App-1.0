fe9b70f92f0d9d44beca05ab674e13c6a1395289
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-02 15:35:58 +0300

    profile

--- Client/src/components/porfile/profile.jsx
+++ Client/src/components/porfile/profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../assets/css/profile.css";
+import im2 from "../../assets/svg/pattern.svg"
+import im3 from "../../assets/svg/user.svg"
 function Profile() {
   return (
     <div>
@@ -7,6 +9,15 @@ function Profile() {
         <h2>Profile</h2>
         <h2>Profile</h2>
       </div>
+
+       <div className="users">
+
+         <img src={im2} className="patterns" width={800} alt="pattern"/>
+         <img src={im3} className="pdp" width={100} alt="users"/>
+             <div className="infoUsers">
+                <h1>Users</h1>
+             </div>
+       </div>
     </div>
   );
 }
