182b3966ce5afc2b4af5454255562460615c54c1
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2023-01-17 20:13:37 +0300

    server

--- Client/src/components/porfile/profile.jsx
+++ Client/src/components/porfile/profile.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import "../../assets/css/profile.css";
-import im2 from "../../assets/svg/pattern.svg"
-import im3 from "../../assets/svg/user.svg"
+import im2 from "../../assets/svg/pattern.svg";
+import im3 from "../../assets/svg/user.svg";
 function Profile() {
   return (
     <div>
-      
-
-       <div className="users">
-
-         <img src={im2} className="patterns" width={800} alt="pattern"/>
-         <img src={im3} className="pdp" width={100} alt="users"/>
-             <div className="infoUsers">
-                <h1>Users</h1>
-             </div>
-       </div>
+      <div className="users">
+        <img src={im2} className="patterns" width={800} alt="pattern" />
+        <img src={im3} className="pdp" width={100} alt="users" />
+        <div className="infoUsers">
+          <h1>Users</h1>
+        </div>
+      </div>
     </div>
   );
 }
